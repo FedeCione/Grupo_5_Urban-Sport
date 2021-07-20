2d7c3dbaed185855ad837f313e539e75cb2b8d96
Elias Kannemann <elias_gk1@hotmail.com>
2021-07-20 16:21:27 -0300

    v 2.0 del productDetail cn header & footer

--- public/javascripts/mainProduct.js
+++ public/javascripts/mainProduct.js
@@ -17,3 +17,11 @@ function slideImage(){
 }
 
 window.addEventListener('resize', slideImage);
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }  

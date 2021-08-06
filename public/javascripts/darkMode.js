811aab9460ebca9ca9c1a5bad6eea1a16dc64dea
FedeCione <63378109+FedeCione@users.noreply.github.com>
2021-08-06 16:22:55 -0300

    Solucionados los problemas con el Dark-Mode

--- public/javascripts/darkMode.js
+++ public/javascripts/darkMode.js
@@ -1,6 +1,10 @@
 let darkMode = document.querySelector('#dark-mode');
+let headerDark = document.querySelector('#header-dark');
+let mainDark = document.querySelector('#main-dark');
 
 darkMode.addEventListener('click', () => {
+    headerDark.classList.toggle('dark');
     document.body.classList.toggle('dark');
+    mainDark.classList.toggle('dark');
     darkMode.classList.toggle('active');
-})
+});

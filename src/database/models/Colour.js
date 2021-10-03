3814078dd6537459e4d22428939c217ebb722d59
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2021-10-03 15:43:53 -0300

    añado el modelo colour a la database

---
+++ src/database/models/Colour.js
@@ -0,0 +1,29 @@
+module.exports=(sequelize,dataTypes)=>{
+
+let alias="Colours"
+let cols={
+    id:{
+        type:dataTypes.INTEGER(11).UNSIGNED,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull:false
+    },
+    name:{
+        type:dataTypes.STRING(100),
+        allowNull:false
+    }
+}
+let config={
+    tableName:"colours",
+    timestamps:false
+}
+let Colour=sequelize.define(alias,cols,config)
+
+Colour.associate =models=>{
+  Colour.hasMany(models.Products,{
+     as:"products",
+     foreignKey:"id_colour"
+  })
+}
+  return Colour;
+}
